7b51ab01e6e7524af040c377fc67698b3bd0bcb6
martires alberto orozco gonzalez <trans_maog@htomail.com>
2018-12-28 08:09:56 -0600

    Print Invoice as PDF in Laravel using JavaScript

--- public/js/app.js
+++ public/js/app.js
@@ -32212,7 +32212,10 @@ var app = new Vue({
   methods: {
     searchit: _.debounce(function () {
       Fire.$emit('searching');
-    }, 1000)
+    }, 1000),
+    printme: function printme() {
+      window.print();
+    }
   }
 });
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -105,7 +105,11 @@ const app = new Vue({
     methods:{
       searchit: _.debounce(() =>{
         Fire.$emit('searching');
-      },1000)
+      },1000),
+
+      printme(){
+         window.print();       
+      }
     
     }
 
